89ed4056c0e9e03104f3ad55e851d3da3eb3695a
pranav1483 <pranavp1483@gmail.com>
2024-01-15 01:11:39 +0530

    Created Message model

---
+++ backend/src/main/java/com/chatapp/backend/model/Message.java
@@ -0,0 +1,53 @@
+package com.chatapp.backend.model;
+
+import java.time.LocalDateTime;
+
+import com.chatapp.backend.enm.MessageType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "message")
+public class Message {
+    
+    @Id
+    @GeneratedValue
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private MessageType type;
+
+    @Column
+    private String contentURL;
+
+    @Column(nullable = false)
+    private boolean oneTime;
+
+    @Column(nullable = false)
+    private Users user_from;
+
+    @Column
+    private Users user_to;
+
+    @Column
+    private Group group_to;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
