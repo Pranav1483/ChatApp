c9906f5fe01b5a1bbd1a25c788d91d7e4fa306d7
pranav1483 <pranavp1483@gmail.com>
2024-01-14 22:06:13 +0530

    Created Group Model

---
+++ backend/src/main/java/com/chatapp/backend/model/Group.java
@@ -0,0 +1,42 @@
+package com.chatapp.backend.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "Group")
+public class Group {
+    
+    @Id
+    @GeneratedValue
+    @Column(updatable = false, nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    @Pattern(regexp = "[A-Za-z0-9_]+")
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false, updatable = false)
+    private Users createdBy;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+}
