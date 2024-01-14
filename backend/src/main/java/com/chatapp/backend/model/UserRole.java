c07144b9212ff2520e3978c30684a54c295c7501
pranav1483 <pranavp1483@gmail.com>
2024-01-15 00:44:53 +0530

    Created UserRole

---
+++ backend/src/main/java/com/chatapp/backend/model/UserRole.java
@@ -0,0 +1,35 @@
+package com.chatapp.backend.model;
+
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "UserRole")
+public class UserRole {
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private Users user;
+
+    @Column(nullable = false)
+    private Group group;
+
+    @Column(nullable = false)
+    private Set<Role> roles;
+}
