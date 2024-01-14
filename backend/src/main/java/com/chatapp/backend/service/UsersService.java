aa67ab9e132fa18410e7169eb220f9ed42b0fdbc
pranav1483 <pranavp1483@gmail.com>
2024-01-15 02:55:10 +0530

    Added save user service

--- backend/src/main/java/com/chatapp/backend/service/UsersService.java
+++ backend/src/main/java/com/chatapp/backend/service/UsersService.java
@@ -1,5 +1,7 @@
 package com.chatapp.backend.service;
 
+import com.chatapp.backend.model.Users;
+
 public interface UsersService {
-    
+    Users saveUser(Users user);
 }
--- backend/src/main/java/com/chatapp/backend/service/impl/UsersServiceImpl.java
+++ backend/src/main/java/com/chatapp/backend/service/impl/UsersServiceImpl.java
@@ -1,10 +1,26 @@
 package com.chatapp.backend.service.impl;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.chatapp.backend.model.Users;
+import com.chatapp.backend.repository.UsersRepository;
 import com.chatapp.backend.service.UsersService;
 
 @Service
 public class UsersServiceImpl implements UsersService{
+
+    private UsersRepository usersRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public UsersServiceImpl(UsersRepository usersRepository, PasswordEncoder passwordEncoder) {
+        this.usersRepository = usersRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Users saveUser(Users user) {
+        
+    }
     
 }
