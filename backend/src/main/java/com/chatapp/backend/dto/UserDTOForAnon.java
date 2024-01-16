3bf6d13602fd6466dd0fc986affb7e1b02ef68d1
pranav1483 <pranavp1483@gmail.com>
2024-01-16 21:33:30 +0530

    Implemented User DTO for Non-Friend Users

--- backend/src/main/java/com/chatapp/backend/dto/UserDTOForAnon.java
+++ backend/src/main/java/com/chatapp/backend/dto/UserDTOForAnon.java
@@ -1,5 +1,24 @@
 package com.chatapp.backend.dto;
 
+import com.chatapp.backend.model.User;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserDTOForAnon {
     
+    private Long id;
+    private String fname;
+    private String lname;
+    private String username;
+
+    public UserDTOForAnon(User user) {
+        this.id = user.getId();
+        this.fname = user.getFname();
+        this.lname = user.getLname();
+        this.username = user.getUsername();
+    }
+
 }
