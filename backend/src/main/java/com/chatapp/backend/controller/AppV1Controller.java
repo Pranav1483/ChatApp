8314bfbc4862642b4f612d27a444c04e98ac8f9d
pranav1483 <pranavp1483@gmail.com>
2024-01-18 22:48:07 +0530

    Added jjwt dependency

--- backend/src/main/java/com/chatapp/backend/controller/AppV1Controller.java
+++ backend/src/main/java/com/chatapp/backend/controller/AppV1Controller.java
@@ -1,11 +1,20 @@
 package com.chatapp.backend.controller;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.chatapp.backend.dto.UserDTOForUser;
+import com.chatapp.backend.model.User;
 import com.chatapp.backend.service.ConnectionService;
 import com.chatapp.backend.service.GroupService;
+import com.chatapp.backend.service.LatestMessageService;
 import com.chatapp.backend.service.MessageReactionService;
 import com.chatapp.backend.service.MessageService;
 import com.chatapp.backend.service.RoleService;
@@ -19,6 +28,7 @@ public class AppV1Controller {
     
     ConnectionService connectionService;
     GroupService groupService;
+    LatestMessageService latestMessageService;
     MessageReactionService messageReactionService;
     MessageService messageService;
     RoleService roleService;
@@ -26,7 +36,8 @@ public class AppV1Controller {
     UserService userService;
 
     public AppV1Controller(ConnectionService connectionService, 
-                            GroupService groupService, 
+                            GroupService groupService,
+                            LatestMessageService latestMessageService, 
                             MessageReactionService messageReactionService, 
                             MessageService messageService, 
                             RoleService roleService,
@@ -34,6 +45,7 @@ public class AppV1Controller {
                             UserService userService) {
                                 this.connectionService = connectionService;
                                 this.groupService = groupService;
+                                this.latestMessageService = latestMessageService;
                                 this.messageReactionService = messageReactionService;
                                 this.messageService = messageService;
                                 this.roleService = roleService;
@@ -41,5 +53,30 @@ public class AppV1Controller {
                                 this.userService = userService;
                             }
     
-
+    
+                            @PostMapping("/user")
+                            public ResponseEntity<?> createUser(@RequestBody User user, BindingResult bindingResult) {
+                                if (bindingResult.hasErrors()) {
+                                    FieldError fieldError = bindingResult.getFieldErrors().get(0);
+                                    String errMsg = fieldError.getDefaultMessage();
+                                    return ResponseEntity.status(400).body(errMsg);
+                                }
+                                try {
+                                    userService.saveUser(user);
+                                    return ResponseEntity.status(200).body(new UserDTOForUser(user));
+                                } catch (DataIntegrityViolationException e) {
+                                    String errMsg = "";
+                                    String violatedField = e.getMessage();
+                                    System.out.println(violatedField);
+                                    if (violatedField.contains("username")) {
+                                        errMsg = "Username already exists";
+                                    }
+                                    else if (violatedField.contains("email")) {
+                                        errMsg = "Email already exists";
+                                    }
+                                    return ResponseEntity.status(409).body(errMsg);
+                                } catch (Exception e) {
+                                    return ResponseEntity.status(500).body("");
+                                }
+                            }
 }
