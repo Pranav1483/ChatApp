11449a76f076ac112677bdabc43329d8f1aa1b39
pranav1483 <pranavp1483@gmail.com>
2024-01-18 22:54:59 +0530

    Constants file

---
+++ backend/src/main/java/com/chatapp/backend/model/Constants.java
@@ -0,0 +1,10 @@
+package com.chatapp.backend.model;
+
+public class Constants {
+    
+    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5*60*60;
+    public static final String SIGNING_KEY = "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+
+}
