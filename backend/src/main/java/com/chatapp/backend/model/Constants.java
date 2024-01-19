3b58aa340ab72ddab79b8c508c92f8c3059d79c9
pranav1483 <pranavp1483@gmail.com>
2024-01-19 12:34:15 +0530

    Changed SECRET_KEY

--- backend/src/main/java/com/chatapp/backend/model/Constants.java
+++ backend/src/main/java/com/chatapp/backend/model/Constants.java
@@ -2,8 +2,8 @@ package com.chatapp.backend.model;
 
 public class Constants {
     
-    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5*60*60;
-    public static final String SIGNING_KEY = "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6";
+    public static final long ACCESS_TOKEN_VALIDITY = 5*60*60*1000;
+    public static final String SIGNING_KEY = "H1CR670IPZ9TCAUSEOTPA0LSZV8H7U4OR0V6GKHYYU6GO7JKO0TC6U1EPOFQ6H7X";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
 
