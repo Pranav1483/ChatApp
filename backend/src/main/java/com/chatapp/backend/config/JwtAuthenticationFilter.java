f4f42315ea7ad226cd5c7b8c7009be4dccc94de4
pranav1483 <pranavp1483@gmail.com>
2024-01-19 09:23:57 +0530

    Implemented AuthFilter

--- backend/src/main/java/com/chatapp/backend/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/chatapp/backend/config/JwtAuthenticationFilter.java
@@ -3,8 +3,12 @@ package com.chatapp.backend.config;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.chatapp.backend.dto.UserDTOForAnon;
 import com.chatapp.backend.model.Constants;
 import com.chatapp.backend.service.UserService;
 
@@ -20,20 +24,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         String header = response.getHeader(Constants.HEADER_STRING);
         String username = null;
-        String authToken = null;
+        String token = null;
         if (header != null && header.startsWith(Constants.TOKEN_PREFIX)) {
-            authToken = header.replace(Constants.TOKEN_PREFIX, "");
-            try {
-                username = jwtTokenUtil.getUsernameFromToken(authToken);
-            } catch (IllegalArgumentException e) {
-                
+            token = header.replace(Constants.TOKEN_PREFIX, "");
+            username = jwtTokenUtil.getUsernameFromToken(token);
+        }
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDTOForAnon userDetails = new UserDTOForAnon(userService.getUserByUsername(username));
+            if (jwtTokenUtil.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null);
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
+        filterChain.doFilter(request, response);
     }
     
 }
