4092a4dcd29d8e296578368023bd4b8cec67124e
pranav1483 <pranavp1483@gmail.com>
2024-01-19 12:10:12 +0530

    Implemented JWT Utils

--- backend/src/main/java/com/chatapp/backend/config/JwtTokenUtil.java
+++ backend/src/main/java/com/chatapp/backend/config/JwtTokenUtil.java
@@ -1,9 +1,64 @@
 package com.chatapp.backend.config;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+
+import com.chatapp.backend.dto.UserDTOForAnon;
+import com.chatapp.backend.model.Constants;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
 public class JwtTokenUtil {
 
-    public String getUsernameFromToken(String authToken) {
-        throw new UnsupportedOperationException("Unimplemented method 'getUsernameFromToken'");
+    private String createToken(Map<String, Object> claims, String username) {
+        return Jwts.builder()
+                    .claims(claims)
+                    .subject(username)
+                    .issuedAt(new Date(System.currentTimeMillis()))
+                    .expiration(new Date(System.currentTimeMillis() + Constants.ACCESS_TOKEN_VALIDITY))
+                    .signWith(getSignKey()).compact();
+    }
+
+    private SecretKey getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(Constants.SIGNING_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().verifyWith(getSignKey()).build().parseSignedClaims(token).getPayload();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String getUsernameFromToken(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Boolean validateToken(String token, UserDTOForAnon userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
     
 }
